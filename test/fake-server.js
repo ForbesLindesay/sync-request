d233836251184cfdb5d508fea012eeb5c9886e10
Forbes Lindesay <forbes@lindesay.co.uk>
2018-01-31 15:12:11 +0000

    TypeScript, FormData and Jest
    [closes #44]
    [closes #86]
    [closes #84]
    
    This converts the library to typescript, adds handling for FormData, factors out the sync-rpc stuff into a separate module, and converts the tests into proper tests running on jest, rather than examples that require manual inspection of the logs.

--- test/fake-server.js
+++ test/fake-server.js
@@ -1,13 +1,13 @@
 'use strict';
 var express = require('express'),
-    bodyParser = require('body-parser'),
-    morgan = require('morgan'),
-    PORT = 3030;
+  bodyParser = require('body-parser'),
+  morgan = require('morgan'),
+  PORT = 3030;
 
 var app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // parse application/json
 app.use(bodyParser.json());
@@ -16,27 +16,27 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 var started = false;
-exports.isStarted = function() { return started };
+exports.isStarted = function() {
+  return started;
+};
 
 var server;
 process.on('message', function(m) {
-    if (m === 'start') {
-        server = app.listen(PORT, function () {
-            started = true;
-            console.log('fake server started', PORT);
-            return process.send('started');// m.cb && setTimeout(m.cb, 1000);
-        });
-    }  else {
-        server.close(function () {
-            started = false;
-            console.log('fake server stopped', PORT);
-            return process.send('closed') && process.exit(0);
-        });
-    }
+  if (m === 'start') {
+    server = app.listen(PORT, function() {
+      started = true;
+      return process.send('started');
+    });
+  } else {
+    server.close(function() {
+      started = false;
+      return process.send('closed') && process.exit(0);
+    });
+  }
 });
 
 ['get', 'post', 'put', 'delete'].forEach(function(method) {
-    app.route('/internal-test')[method](function(req, res){
-        res.send('ok');
-    });
+  app.route('/internal-test')[method](function(req, res) {
+    res.send('ok');
+  });
 });
